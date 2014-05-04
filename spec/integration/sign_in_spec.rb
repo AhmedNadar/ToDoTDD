5d296eb79abc6b335c961c49e9cd2783eda297bb
Ahmed Nadar <ahmedfay@hotmail.com>
2014-05-03 20:56:02 -0400

    Refactor for specs and Todo controller

--- spec/integration/sign_in_spec.rb
+++ spec/integration/sign_in_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature "Sign in as a user" do
   scenario "with an email address" do
-    visit root_path
-    fill_in "Email address", with: "ahmed@mail.com"
-    click_button "Sign in"
-
-    expect(page).to have_css '.welcome', text: 'Welcome, ahmed@mail.com'
-  end
+		sign_in_as 'me@mail.com'
+    expect(page).to have_css '.welcome', text: 'Welcome, me@mail.com'
+  end 
 end
