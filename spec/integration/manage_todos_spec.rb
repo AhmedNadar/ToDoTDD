bb396abb964463868091eed5b8bf9855cc0ed1b9
Ahmed Nadar <ahmedfay@hotmail.com>
2014-05-07 21:17:34 -0400

    Add completion for Todos

--- spec/integration/manage_todos_spec.rb
+++ spec/integration/manage_todos_spec.rb
@@ -3,7 +3,7 @@ feature "Manage todos" do
   scenario "create a new todo" do
   	sign_in
     create_todo_with_description "Buy some milk"
-		users_sees_todo_item "Buy some milk"
+		user_sees_todo_item "Buy some milk"
   end
 
   scenario "view only my todos" do
@@ -14,16 +14,23 @@ feature "Manage todos" do
     user_does_not_sees_todo_item "Buy some eggs"
   end
 
+  scenario "mark todos as complete" do
+    sign_in
+    create_todo_with_description "Buy some milk"
+    within 'li.todo' do
+    	click_link 'Complete'
+    end
+    expect(page).to have_css 'li.todo.completed'
+  end
+
   def create_todo_with_description(description)
     click_link "Add a new todo"
     fill_in "Description", with: description
     click_button "Create todo"  	
   end
- 
   def user_sees_todo_item(description)
   	expect(page).to have_css 'li.todo', text: description
   end
-
   def user_does_not_sees_todo_item(description)
   	expect(page).not_to have_css 'li.todo', text: description
   end
